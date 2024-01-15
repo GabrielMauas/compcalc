bdea91787a4b880cd2b1969306bd5411d0e32a41
Gabriel Mauas <gabymauas02@gmail.com>
2024-01-15 11:23:44 -0300

    feat: add SpeedInsights component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -38,6 +40,7 @@ export default function RootLayout({ children }) {
       <link rel="icon" href="/compcalclogo.svg" sizes="any" />
       <body className={inter.className}>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
